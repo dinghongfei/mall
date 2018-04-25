6d663969579962f15e0524c04e26a69010b6e47f
dinghongfei <dhf0710@163.com>
2018-04-25 17:41:10 +0800

    v2.0*****StringUtils包引用修改

--- src/main/java/com/mall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/mall/service/impl/OrderServiceImpl.java
@@ -29,7 +29,7 @@ import com.mall.vo.OrderVo;
 import com.mall.vo.ShippingVo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
